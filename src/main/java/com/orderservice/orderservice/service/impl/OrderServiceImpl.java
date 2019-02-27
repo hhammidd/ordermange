f24ecb588b18aa5b2546ea32a9a39c2719f3296c
hamid <hamid.shafikani@gmail.com>
2019-02-27 17:23:13 +0100

    xx

--- src/main/java/com/orderservice/orderservice/service/impl/OrderServiceImpl.java
+++ src/main/java/com/orderservice/orderservice/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.orderservice.orderservice.service.impl;
 
+import com.orderservice.orderservice.dto.CustomerOrderDetailDto;
 import com.orderservice.orderservice.dto.OrderListDto;
 import com.orderservice.orderservice.model.Orders;
 import com.orderservice.orderservice.model.dao.OrderRepository;
@@ -17,10 +18,36 @@ public class OrderServiceImpl implements OrderService {
     private OrderRepository orderRepository;
 
     @Override
-    public List<OrderListDto> getListOrderForCustomer(int customerId) {
+    public List<OrderListDto> getListOfOrdersForCustomer(int customerId) {
         List<OrderListDto> orderListDtos = new ArrayList<>();
         List<Orders> ordersList = orderRepository.findAll();
 
         return orderListDtos;
+    }
+
+    @Override
+    public List<CustomerOrderDetailDto>  getCustomerOrderDetail(int customerId) {
+        List<CustomerOrderDetailDto> customerOrderDetailDtoList = null;
+        List<Orders> orderCustomerList = null;
+
+        orderCustomerList = orderRepository.findByIdAndCustomer(customerId);
+
+        // TODO convert Orders List to CustomerOrderDetailDto List
+        customerOrderDetailDtoList = convertToCustomerOrderDto(orderCustomerList);
+        // TODO put the
+        return customerOrderDetailDtoList;
+    }
+
+    private List<CustomerOrderDetailDto> convertToCustomerOrderDto(List<Orders> orderCustomerList) {
+        List<CustomerOrderDetailDto> customerOrderDetailDtoList = new ArrayList<>();
+
+        for (Orders orderObj : orderCustomerList){
+            CustomerOrderDetailDto customerOrderDetailDtoObj = new CustomerOrderDetailDto();
+            customerOrderDetailDtoObj.setOrderDate(orderObj.getRegistrationDate());
+
+            customerOrderDetailDtoList.add(customerOrderDetailDtoObj);
+
+        }
+        return customerOrderDetailDtoList;
     }
 }
