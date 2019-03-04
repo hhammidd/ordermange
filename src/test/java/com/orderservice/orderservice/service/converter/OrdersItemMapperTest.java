9f3feedb5708888df6e5b1dcc3d41e3b18fbfbf3
hamid <hamid.shafikani@gmail.com>
2019-03-04 08:58:14 +0100

    new oner

--- src/test/java/com/orderservice/orderservice/service/converter/OrdersItemMapperTest.java
+++ src/test/java/com/orderservice/orderservice/service/converter/OrdersItemMapperTest.java
@@ -5,25 +5,13 @@ import com.orderservice.orderservice.model.OrdersItem;
 import com.orderservice.orderservice.model.Production;
 import org.springframework.stereotype.Component;
 
-@Component
-public class OrdersItemMapperTest implements Mapper<OrdersItem, OrdersItemTo> {
-    @Override
-    public OrdersItemTo convertToDto(OrdersItem ordersItem) {
-        OrdersItemTo to = new OrdersItemTo();
-        to.setId(ordersItem.getId());
-        to.setQuantity(ordersItem.getQuantity());
-        to.setProductionId(ordersItem.getProduction().getId());
-        return to;
+
+public class OrdersItemMapperTest {
+    public void convertToDtoTest() {
+
     }
 
-    @Override
-    public OrdersItem convertToDomain(OrdersItemTo ordersItemTo) {
-        OrdersItem i = new OrdersItem();
-        i.setId(ordersItemTo.getId());
-        i.setQuantity(ordersItemTo.getQuantity());
-        Production production = new Production();
-        production.setId(ordersItemTo.getProductionId());
-        i.setProduction(production);
-        return i;
+    public void convertToDomainTest() {
+
     }
 }
